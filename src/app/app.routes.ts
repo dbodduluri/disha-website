1f989e697c8617f12b327b4761a0f7aff90af43d
dishabod <dishabod@buffalo.edu>
2025-07-15 14:57:09 -0400

    project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {Home} from './pages/home/home';
+import {About} from './pages/about/about';
+import { Experience } from './pages/experience/experience';
+import { Resume } from './pages/resume/resume';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+    {path:'', component:Home},
+    {path:'about', component:About},
+    {path:'experience', component:Experience},
+    {path:'resume',component:Resume},
+    {path:'contact', component:Contact},
+];
+
---
+++ src/app/pages/experience/experience.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience.html',
+  styleUrls: ['./experience.css']
+})
+export class Experience {
+
+}
